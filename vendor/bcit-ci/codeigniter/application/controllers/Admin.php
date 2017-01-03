55aefbc796aa7f5b6b239cb6d1e5a1682a2c483c
Scroph <azi.hassan@live.fr>
2017-01-03 23:35:35 +0000

    Deleted Vim swap files

--- vendor/bcit-ci/codeigniter/application/controllers/Admin.php
+++ vendor/bcit-ci/codeigniter/application/controllers/Admin.php
@@ -6,7 +6,6 @@ class Admin extends CI_Controller
 	public function __construct()
 	{
 		parent::__construct();
-		$this->load->library('session');
 		$this->load->library('form_validation');
 		$this->load->helper(array('form', 'url'));
 	}
